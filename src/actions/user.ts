e95f12c10ec4ac5c3781e27974c6e9764be29186
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-02-21 23:49:16 +0530

    created user server action functionality

---
+++ src/actions/user.ts
@@ -0,0 +1,71 @@
+"use sever";
+
+import { currentUser } from "@clerk/nextjs/server";
+import { client } from "@/lib/prisma";
+
+export const onAuthenticateUser = async () => {
+  try {
+    const user = await currentUser();
+
+    if (!user) {
+      return {
+        status: 403,
+      };
+    }
+
+    const existingUser = await client.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+      include: {
+        workSpace: {
+          where: {
+            User: {
+              clerkId: user.id,
+            },
+          },
+        },
+      },
+    });
+    if (existingUser) {
+      return { satus: 200, user: existingUser };
+    }
+
+    const newUser = await client.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        image: user.imageUrl,
+        studio: {
+          create: {},
+        },
+        subcription: {
+          create: {},
+        },
+        workSpace: {
+          create: {
+            name: `${user.firstName}'s Workspace`,
+            type: "PERSONAL",
+          },
+        },
+      },
+      include: {
+        workSpace: true,
+        subcription: {
+          select: {
+            plan: true,
+          },
+        },
+      },
+    });
+    if (newUser) {
+      return { status: 200, user: newUser };
+    }
+    return { status: 400 };
+  } catch (error) {
+    console.log("onAuthenticateUser", error);
+    return { status: 400 };
+  }
+};
