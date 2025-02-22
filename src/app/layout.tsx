826d38b467d021125daea0cbae70f8a9ab0569a9
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-02-22 17:46:02 +0530

    created react query provider component and wrapper entire root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-
 import type { Metadata } from "next";
 import "./globals.css";
 import { Manrope } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import ReactQueryProvider from "@/react-query";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -20,7 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${manrope.className} bg-[#171717] text-white `}>
-        {children}
+          <ReactQueryProvider>{children}</ReactQueryProvider>
         </body>
       </html>
     </ClerkProvider>
