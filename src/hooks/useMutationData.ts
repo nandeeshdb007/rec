e1e5d27cdc8917ae80fe62c433baee48206f567a
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-03-06 23:22:38 +0530

    feat: created-useMutationData-custom-hook

---
+++ src/hooks/useMutationData.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  MutationFunction,
+  MutationKey,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { toast } from "sonner";
+
+export const useMutationData = (
+  mutationKey: MutationKey,
+  mutationFn: MutationFunction<any, any>,
+  queryKey?: string,
+  onSuccess?: () => void
+) => {
+  const client = useQueryClient();
+  const { mutate, isPending } = useMutation({
+    mutationKey,
+    mutationFn,
+    onSuccess(data: any) {
+      if (onSuccess) onSuccess();
+      return toast(data?.status == 200 ? "Success" : "Error", {
+        description: data?.data,
+      });
+    },
+    onSettled: async () => {
+      return await client.invalidateQueries({ queryKey: [queryKey] });
+    },
+  });
+
+  return {
+    mutate,
+    isPending,
+  };
+};
