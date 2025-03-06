d0d5edb3672e38137eee75b84f0fd06588cdc904
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-03-07 00:35:53 +0530

    feat: created-dynamic-side-bar-ui-component

---
+++ src/components/global/sidebar/sidebar-item.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+type Props = {
+  icon: React.ReactNode;
+  title: string;
+  href: string;
+  selected: boolean;
+  notification?: number;
+};
+
+const SideBarItem = ({ icon, title, href, selected, notification }: Props) => {
+  return (
+    <li className="cursor-pointer my-1.5">
+      <Link
+        href={href}
+        className={cn(
+          "flex items-center justify-between group rounded-lg hover:bg-[#1d1d1d]",
+          selected ? "bg-[#1d1d1d]" : ""
+        )}
+      >
+        <div className="flex items-center gap-2 transition-all p-1.5 cursor-pointer">
+          {icon}
+          <span
+            className={cn(
+              "font-medium group-hover:text[#9d9d9d] transition-all truncate w-32",
+              selected ? "text-[#9d9d9d]" : "text-[#545454]"
+            )}
+          >
+            {title}
+          </span>
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default SideBarItem;
