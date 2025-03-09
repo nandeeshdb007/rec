2d240e0d6f0861105d942769ad64d15dee0fa0a9
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-03-09 12:00:41 +0530

    hot-fix: fixed-info-bar-ui

--- src/components/global/info-bar/index.tsx
+++ src/components/global/info-bar/index.tsx
@@ -12,11 +12,11 @@ const InfoBar = (props: Props) => {
       <div className="flex gap-4 justify-center items-center border-2 rounded-full px-4 w-full max-w-lg">
         <Search size={25} className="text-[#707070]" />
         <Input
-          className="bg-transparent border-none !placeholder-neutral-500 !outline-none focus:border-none` focus:outline-none"
+          className="bg-transparent  border-none !placeholder-neutral-500 !outline-none focus:border-none` focus:outline-none"
           placeholder="Search for people, projects, tags and folders"
         />
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4  px-1 lg:px-4">
         <Button className="bg-white text-black hover:bg-[#9d9d9d]  flex items-center gap-2">
           <UploadIcon size={30} />{" "}
           <span className="flex items-center gap-2">Upload</span>
