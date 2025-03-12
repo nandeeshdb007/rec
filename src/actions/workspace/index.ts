a13caca32abb6d802a815a465f9288c21160f5d7
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-03-12 22:24:01 +0530

    feat: created-getFolderInfo-server-action

--- src/actions/workspace/index.ts
+++ src/actions/workspace/index.ts
@@ -243,3 +243,31 @@ export const createFolder = async (workspaceId: string) => {
     return { status: 500, message: error };
   }
 };
+
+export const getFolderInfo = async (folderId: string) => {
+  try {
+    const folder = await client.folder.findUnique({
+      where: {
+        id: folderId,
+      },
+      select: {
+        name: true,
+        _count: {
+          select: {
+            videos: true,
+          },
+        },
+      },
+    });
+    if (folder) {
+      return {
+        status: 200,
+        data: folder,
+      };
+    }
+    return { status: 400, data:null };
+
+  } catch (error) {
+    return { status: 500, message: error };
+  }
+};
