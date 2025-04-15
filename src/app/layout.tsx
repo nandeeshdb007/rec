cb7156e661abbab50b88048f70dad1ff10bb24fa
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-04-16 00:46:10 +0530

    fix: wrappend-entire-project-in-redux-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Manrope } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import ReactQueryProvider from "@/react-query";
+import { ReduxProvider } from "@/redux/provider";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${manrope.className} bg-[#171717] text-white `}>
-          <ReactQueryProvider>{children}</ReactQueryProvider>
+          <ReduxProvider>
+            <ReactQueryProvider>{children}</ReactQueryProvider>
+          </ReduxProvider>
         </body>
       </html>
     </ClerkProvider>
